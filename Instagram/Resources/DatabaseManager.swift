89f7ba92643df81b72628a6eacc92cecffbf2e37
Ethan Borrowman <ethan_borrowman@hotmail.com>
2021-10-13 22:37:44 -0400

    Firebase integration, starting login and registration flow

---
+++ Instagram/Resources/DatabaseManager.swift
@@ -0,0 +1,16 @@
+//
+//  DatabaseManager.swift
+//  Instagram
+//
+//  Created by Ethan Borrowman on 10/13/21.
+//
+
+import Foundation
+import FirebaseDatabase
+
+public class DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    // MARK: - Public
+    
+}
---
+++ Instagram/Resources/StorageManager.swift
@@ -0,0 +1,15 @@
+//
+//  StorageManager.swift
+//  Instagram
+//
+//  Created by Ethan Borrowman on 10/13/21.
+//
+
+import Foundation
+import FirebaseStorage
+
+public class StorageManager {
+    static let shared = StorageManager()
+    
+    // MARK: - Public
+}
